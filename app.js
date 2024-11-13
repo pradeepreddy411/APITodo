d42690b7c55f04184180e67612a30c9527d56c8b
pradeep <pradeepreddy411@gmail.com>
2024-11-13 09:59:47 +0000

    TODO-ASSISSMENT

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const initializeDbAndServer = async () => {
       driver: sqlite3.Database,
     });
     app.listen(3000, () =>
-      console.log("Server Running at http://localhost:3000/")
+      console.log("Server Running at http://localhost:3000")
     );
   } catch (e) {
     console.log(`DB Error:${e.message}`);
@@ -24,68 +24,94 @@ const initializeDbAndServer = async () => {
 };
 initializeDbAndServer();
 
-const hasPriorityAndStatusProperties = (requestQuery) => {
-  return (
-    requestQuery.priority !== undefined && requestQuery.status !== undefined
-  );
+const authentication = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken) {
+    jwt.verify(jwtToken, "SECRET_KEY", (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        request.username = payload.username;
+        request.userId = payload.userId;
+        next();
+      }
+    });
+  } else {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  }
 };
 
-const hasPriorityProperty = (requestQuery) => {
-  return requestQuery.priority !== undefined;
-};
+app.post("/register/", async (request, response) => {
+  const { email, password, name } = request.body;
+  const getUserQuery = `SELECT * FROM user WHERE email='${email}'`;
+  const userDBDetails = await db.get(getUserQuery);
+  if (userDBDetails !== undefined) {
+    response.status(400);
+    response.send("User already exists");
+  } else {
+    if (password.length < 6) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const createUserQuery = `INSERT INTO user(email,password,name)
+        VALUES ('${email}','${hashedPassword}','${name}', )`;
+      await db.run(createUserQuery);
+      response.send("User created successfully");
+    }
+  }
+});
+
+app.post("/login", async (request, response) => {
+  const { email, password } = request.body;
+  const getUserQuery = `SELECT * FROM user WHERE email='${email}';`;
+  const userDbDetails = await db.get(getUserQuery);
+
+  if (userDbDetails !== undefined) {
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      userDbDetails.password
+    );
+    if (isPasswordCorrect) {
+      const payload = { email, userId: userDbDetails.user_id };
+      const jwtToken = jwt.sign(payload, "SECRET_KEY");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  } else {
+    response.status(400);
+    response.send("Invalid email");
+  }
+});
 
 const hasStatusProperty = (requestQuery) => {
   return requestQuery.status !== undefined;
 };
 
-app.get("/todos/", async (request, response) => {
+app.get("/todos/", authentication, async (request, response) => {
   let data = null;
   let getTodosQuery = "";
-  const { search_q = "", priority, status } = request.query;
-
-  switch (true) {
-    case hasPriorityAndStatusProperties(request.query):
-      getTodosQuery = `
-            SELECT
-            *
-             FROM
-             todo
-             
-             WHERE
-             todo LIKE '%${search_q}%'
-             AND status='${status}'
-             AND priority='${priority}';`;
-
-      break;
-
-    case hasPriorityProperty(request.query):
-      getTodosQuery = `
-            SELECT * FROM todo
-            WHERE
-            todo LIKE '%${search_q}%'
-            AND priority='${priority}';`;
-
-      break;
-
-    case hasStatusProperty(request.query):
-      getTodosQuery = `
+  const { status } = request.query;
+  const requestQuery = request.query;
+  if (requestQuery.status !== undefined)
+    getTodosQuery = `
             SELECT * FROM todo 
             WHERE
-            todo LIKE '%${search_q}%'
-            AND status='${status}';`;
-      break;
-
-    default:
-      getTodosQuery = `
-            SELECT * FROM todo WHERE todo LIKE '%${search_q}%';`;
-      break;
-  }
+           status='${status}';`;
 
   data = await db.all(getTodosQuery);
   response.send(data);
 });
 
-app.get("/todos/:todoId/", async (request, response) => {
+app.get("/todos/:todoId/", authentication, async (request, response) => {
   const { todoId } = request.params;
   const getTodoQuery = `
     SELECT * FROM todo WHERE id=${todoId};`;
@@ -93,57 +119,45 @@ app.get("/todos/:todoId/", async (request, response) => {
   response.send(todo);
 });
 
-app.post("/todos/", async (request, response) => {
-  const { id, todo, priority, status } = request.body;
+app.post("/todos/", authentication, async (request, response) => {
+  const { id, status } = request.body;
   const postTodoQuery = `
     INSERT INTO
-    todo(id,todo,priority,status)
+    todo(id,status)
     VALUES
-    (${id},'${todo}','${priority}','${status}');`;
+    (${id},'${status}');`;
   await db.run(postTodoQuery);
   response.send("Todo Successfully Added");
 });
 
-app.put("/todos/:todoId/", async (request, response) => {
+app.put("/todos/:todoId/", authentication, async (request, response) => {
   const { todoId } = request.params;
   let updateColumn = "";
-  const requestBody = request.body;
-  switch (true) {
-    case requestBody.status !== undefined:
-      updateColumn = "Status";
-      break;
 
-    case requestBody.priority !== undefined:
-      updateColumn = "Priority";
-      break;
-
-    case requestBody.todo !== undefined:
-      updateColumn = "Todo";
-      break;
-  }
   const previousTodoQuery = `
     SELECT * FROM todo WHERE id=${todoId};`;
   const previousTodo = await db.get(previousTodoQuery);
 
-  const {
-    todo = previousTodo.todo,
-    priority = previousTodo.priority,
-    status = previousTodo.status,
-  } = request.body;
+  const { status = previousTodo.status } = request.body;
+
+  let updateTodoQuery;
+
+  const requestBody = request.body;
+
+  if (requestBody.status !== undefined) updateColumn = "Status";
+
   const updateTodoQuery = `
-    UPDATE
-    todo
-    SET
-    todo='${todo}',
-    priority='${priority}',
-    status='${status}'
-    WHERE
-    id=${todoId};`;
+            UPDATE
+            todo
+            SET
+            status='${status}'
+            WHERE
+            id=${todoId};`;
   await db.run(updateTodoQuery);
   response.send(`${updateColumn} Updated`);
 });
 
-app.delete("/todos/:todoId/", async (request, response) => {
+app.delete("/todos/:todoId/", authentication, async (request, response) => {
   const { todoId } = request.params;
   const deleteTodoQuery = `
     DELETE FROM todo WHERE id=${todoId};`;
